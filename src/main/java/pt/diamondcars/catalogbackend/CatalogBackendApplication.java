84a5697b199ecd7254c6c360a5eb111be47db0f7
opereira24 <goncalocpereira24@gmail.com>
2026-09-03 13:55:06 +0100

    chore: initial Spring Boot scaffold for catalog-backend

---
+++ src/main/java/pt/diamondcars/catalogbackend/CatalogBackendApplication.java
@@ -0,0 +1,13 @@
+package pt.diamondcars.catalogbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogBackendApplication.class, args);
+	}
+
+}
